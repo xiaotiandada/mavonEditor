c6c2313d6076d5271662fb30b933e18114761dc9
xiaotian <952822399@qq.com>
2020-05-08 11:16:38 +0800

    build

--- src/lib/extmarkdown.js
+++ src/lib/extmarkdown.js
@@ -1,24 +1,24 @@
 const tokenizeRules = [
     {
         regexp: /[^\[]+/mg,
-        block: 'text',
+        block: 'text'
     },
     {
         regexp: /^\[read[^\]]+\](?!\()/mg,
-        block: 'readOpen',
+        block: 'readOpen'
     },
     {
         regexp: /^\[\/read\](?!\()/mg,
-        block: 'readClose',
+        block: 'readClose'
     },
     {
         regexp: /^\[else\](?!\()/mg,
-        block: 'else',
+        block: 'else'
     },
     {
         regexp: /\[/mg,
-        block: 'text',
-    },
+        block: 'text'
+    }
 ];
 
 const lockSvg = '<svg width="10" height="12" xmlns="http://www.w3.org/2000/svg"><path style="fill: white" d="M4.59.011c.395 0 .776.074 1.143.223.366.148.692.352.976.611.284.26.513.563.686.909.173.346.26.716.26 1.112v1.829H6.158V3.212c0-.52-.146-.923-.439-1.211-.292-.289-.686-.433-1.18-.433-.445 0-.803.144-1.075.433-.272.288-.408.692-.408 1.21V4.72H1.5V2.866c0-.396.078-.766.235-1.112.156-.346.373-.65.649-.909.276-.26.601-.463.976-.611A3.314 3.314 0 014.59.01zM9.271 8.44V10.082c0 .223-.039.433-.117.631a1.521 1.521 0 01-.321.507 1.489 1.489 0 01-1.094.457H1.487c-.223 0-.424-.04-.606-.117a1.401 1.401 0 01-.772-.797 1.598 1.598 0 01-.105-.581v-3.93c0-.23.08-.424.24-.58a.78.78 0 01.563-.236h7.65c.23 0 .424.079.58.235.157.157.235.35.235.58V8.44z"/></svg>'
@@ -46,25 +46,25 @@ export function formatReadTags(content, displayMode) {
 
 function render(innerText, elseText, hold, displayMode) {
     let html = '';
-    if(displayMode === 0 || displayMode === 1) {
-        html += `<div class="unlock-content${displayMode === 1 ? ' alone-show' : ''}">\n`
-            + `<div class="lock-bg">\n`
-            + unlockSvg
-            + `</div>\n`
-            + innerText
-            + `</div>`;
+    if (displayMode === 0 || displayMode === 1) {
+        html += `<div class="unlock-content${displayMode === 1 ? ' alone-show' : ''}">\n` +
+            `<div class="lock-bg">\n` +
+            unlockSvg +
+            `</div>\n` +
+            innerText +
+            `</div>`;
     }
-    if(displayMode === 0 || displayMode === 2) {
+    if (displayMode === 0 || displayMode === 2) {
         const list = conditionList(hold);
-        if(!html) html += '\n'
-        html += `<div class="unlock-prompt${displayMode === 2 ? ' alone-show' : ''}">\n`
-            + `<div class="lock-bg">\n`
-            + lockSvg
-            + `</div>\n`
-            + (elseText && elseText.trim() !== 'Hidden content' ? elseText + '\n<hr />' : '')
-            + `<div>\n<h4 class="condition-title">\n隐藏内容，满足以下条件解锁:\n</h4>\n`
-            + list
-            + `</div>\n</div>`;
+        if (!html) html += '\n'
+        html += `<div class="unlock-prompt${displayMode === 2 ? ' alone-show' : ''}">\n` +
+            `<div class="lock-bg">\n` +
+            lockSvg +
+            `</div>\n` +
+            (elseText && elseText.trim() !== 'Hidden content' ? elseText + '\n<hr />' : '') +
+            `<div>\n<h4 class="condition-title">\n隐藏内容，满足以下条件解锁:\n</h4>\n` +
+            list +
+            `</div>\n</div>`;
     }
     return html;
 }
@@ -72,11 +72,11 @@ function render(innerText, elseText, hold, displayMode) {
 function conditionList(need) {
     // 条件列表
     let list = ''
-    for(let i = 0; i < need.length; i++) {
-    list += `\n<div class="condition fl">`
-        + `<p style="flex: 1">`
-        + `持有：${need[i].amount / 10000 } ${need[i].token}`
-        + `</p>\n</div>`
+    for (let i = 0; i < need.length; i++) {
+    list += `\n<div class="condition fl">` +
+        `<p style="flex: 1">` +
+        `持有：${need[i].amount / 10000} ${need[i].token}` +
+        `</p>\n</div>`
     }
     return list
 }
@@ -86,12 +86,11 @@ function parse(text) {
     let α = 0,
         β = [];
     while (α < tokenized.length) {
-
         if (lookseq(tokenized, α, 'readOpen', 'text', 'readClose')) {
             β.push({
                 block: 'read',
                 innerText: tokenized[α + 1].value,
-                attributes: parseReadOpen(tokenized[α]),
+                attributes: parseReadOpen(tokenized[α])
             });
             α += 3;
             continue;
@@ -103,7 +102,7 @@ function parse(text) {
                 block: 'read',
                 innerText: tokenized[α + 1].value,
                 elseText: tokenized[α + 3].value,
-                attributes: parseReadOpen(tokenized[α]),
+                attributes: parseReadOpen(tokenized[α])
             });
             α += 5;
             continue;
@@ -201,4 +200,3 @@ function attrMines(val) {
     }
     return β;
 }
-
